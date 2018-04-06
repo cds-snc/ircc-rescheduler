f90481b57a673a671d9838d294e115409a1b07fe
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2018-04-06 17:14:23 -0400

    Add react-day-picker

---
+++ src/Calendar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import DayPicker, { DateUtils } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+
+export default class Calendar extends Component {
+  constructor(props) {
+    super(props)
+    this.handleDayClick = this.handleDayClick.bind(this)
+    this.state = {
+      selectedDays: [],
+    }
+  }
+  handleDayClick(day, { selected }) {
+    const { selectedDays } = this.state
+    if (selected) {
+      const selectedIndex = selectedDays.findIndex(selectedDay =>
+        DateUtils.isSameDay(selectedDay, day),
+      )
+      selectedDays.splice(selectedIndex, 1)
+    } else {
+      selectedDays.push(day)
+    }
+    this.setState({ selectedDays })
+  }
+  render() {
+    return (
+      <div>
+        <DayPicker
+          numberOfMonths={2}
+          disabledDays={[{ daysOfWeek: [0, 6] }]}
+          pagedNavigation
+          fixedWeeks
+          selectedDays={this.state.selectedDays}
+          onDayClick={this.handleDayClick}
+        />
+      </div>
+    )
+  }
+}
