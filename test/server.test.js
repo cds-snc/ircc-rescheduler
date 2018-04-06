88eea3cd381fc34774b5a9ac5e1789be816b7fff
nmakuch <makuch.nick@gmail.com>
2018-04-06 15:25:31 -0400

    Page-wide styling + tests

--- test/server.test.js
+++ test/server.test.js
@@ -9,17 +9,19 @@ describe('Server Side Rendering', () => {
   it('renders the register page by default', async () => {
     let response = await request(server).get('/')
 
-    expect(response.text).toMatch(/Register/)
+    expect(response.text).toMatch(/Full name/)
   })
 
   it('renders the calendar at /calendar', async () => {
     let response = await request(server).get('/calendar')
-    expect(response.text).toMatch(/Calendar/)
+    expect(response.text).toMatch(
+      /select all the days you’re AVAILABLE between May and June 2018/,
+    )
   })
 
   it('renders availability information at /availability', async () => {
     let response = await request(server).get('/availability')
-    expect(response.text).toMatch(/Availability/)
+    expect(response.text).toMatch(/When is the <strong>most convenient time/)
   })
 
   it('renders a reassuring confirmation message at /confirmation', async () => {
