8521b6e1e6841f3dd5010f09491ad151abc781d6
Mike Williamson <mike@korora.ca>
2018-04-13 17:40:48 -0400

    Get tests working with Apollo Link State

--- src/__tests__/FederalBanner.test.js
+++ src/__tests__/FederalBanner.test.js
@@ -1,10 +1,41 @@
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { ApolloProvider } from 'react-apollo'
 import FederalBanner from '../FederalBanner'
 import React from 'react'
+import { ApolloLink } from 'apollo-link'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { withClientState } from 'apollo-link-state'
+// TODO: switch the project over to unfetch.
+import fetch from 'unfetch' // eslint-disable-line no-unused-vars
+
+const cache = new InMemoryCache()
+const stateLink = withClientState({
+  cache,
+  defaults: {
+    language: 'en',
+  },
+  typeDefs: `
+    type Query {
+      language: String
+    }
+`,
+})
+
+const client = new ApolloClient({
+  ssrMode: true,
+  link: ApolloLink.from([stateLink, new HttpLink({ fetch })]),
+  cache,
+})
 
 describe('<FederalBanner />', () => {
   it('renders', () => {
-    const federalBanner = shallow(<FederalBanner />)
+    const federalBanner = mount(
+      <ApolloProvider client={client}>
+        <FederalBanner />
+      </ApolloProvider>,
+    )
     expect(federalBanner.find('svg')).toBeTruthy()
   })
 })
