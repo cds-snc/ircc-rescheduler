b88f3a0819c3948badabab9b4fd8bb92c03d5c99
nmakuch <makuch.nick@gmail.com>
2018-04-13 11:31:01 -0400

    404 page setup

---
+++ src/404Page.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { H1, H2, ErrorContent, ErrorMessage } from './styles'
+import { NavLink } from 'react-router-dom'
+import PageHeader from './PageHeader'
+import AlphaBanner from './AlphaBanner'
+import FederalBanner from './FederalBanner'
+import Footer from './Footer'
+
+class TestPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <AlphaBanner>
+          {' '}
+          <span>This is a new service we are constantly improving.</span>{' '}
+        </AlphaBanner>
+        <FederalBanner />
+        <main role="main">
+          <PageHeader>
+            <H1>Request a new Canadian Citizenship test date</H1>
+          </PageHeader>
+
+          <ErrorContent>
+            <H1> 404: Page not found </H1>
+            <H2>
+              {' '}
+              Oops! We cant seem to find the page you are looking for:
+            </H2>{' '}
+            <ErrorMessage>
+              If you wish to restart the rescheduling process, please navigate
+              to the Home Page and start the process again.{' '}
+            </ErrorMessage>
+            <NavLink to="/">
+              <p>← Go Back</p>
+            </NavLink>
+          </ErrorContent>
+
+          <Footer topBarBackground="black" />
+        </main>
+      </div>
+    )
+  }
+}
+
+export default TestPage
