b4e6a70528b13df9496988f0d127bc0378747081
Paul Craig <paul_craig_16@hotmail.com>
2018-04-15 16:07:06 -0400

    Add tests for RadioAdaptor component

--- src/forms/__tests__/MultipleChoice.test.js
+++ src/forms/__tests__/MultipleChoice.test.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { render, shallow } from 'enzyme'
-import { Radio } from '../MultipleChoice'
+import { Radio, RadioAdapter } from '../MultipleChoice'
 
 const defaultProps = {
   label: <span>Option</span>,
   value: 'option',
 }
+
+const getAdapterAttrib = (adapter, attr) =>
+  adapter.find('input')[0].attribs[attr]
 
 describe('<Radio> component', () => {
   it('has props assigned correctly', () => {
@@ -18,5 +21,20 @@ describe('<Radio> component', () => {
   it('renders label correctly', () => {
     const radio = render(<Radio {...defaultProps} />)
     expect(radio.find('label > span').text()).toMatch(/Option/)
+  })
+})
+
+describe('<RadioAdapter> component', () => {
+  it('has props assigned directly through input object', () => {
+    const radioAdapterProps = {
+      input: { value: defaultProps.value },
+      label: defaultProps.label,
+    }
+    const radioAdapter = render(<RadioAdapter {...radioAdapterProps} />)
+    expect(radioAdapter.find('input')).toBeTruthy()
+
+    const radio = shallow(<Radio {...defaultProps} />)
+    expect(getAdapterAttrib(radioAdapter, 'type')).toEqual(radio.props().type)
+    expect(getAdapterAttrib(radioAdapter, 'value')).toEqual(radio.props().value)
   })
 })
