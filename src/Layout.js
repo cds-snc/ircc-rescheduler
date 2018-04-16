43a9a552162ca69366a05ba19363a095e6cb1a50
Paul Craig <paul_craig_16@hotmail.com>
2018-04-15 23:57:43 -0400

    Create Layout component
    Our pages all have common headers and footers, so some kind of
    Layout abstraction will help us maintain that consistency.

---
+++ src/Layout.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Trans } from 'lingui-react'
+import { H1, Content } from './styles'
+import PageHeader from './PageHeader'
+import AlphaBanner from './AlphaBanner'
+import FederalBanner from './FederalBanner'
+import Footer from './Footer'
+
+const Layout = ({ children, mainClassName = '' }) => (
+  <div>
+    <AlphaBanner>
+      <span>
+        <Trans>This is a new service we are constantly improving.</Trans>
+      </span>
+    </AlphaBanner>
+    <FederalBanner />
+    <main role="main" className={mainClassName}>
+      <PageHeader>
+        <H1>
+          <Trans>Request a new Canadian Citizenship test date</Trans>
+        </H1>
+      </PageHeader>
+      <Content>{children}</Content>
+      <Footer topBarBackground="black" />
+    </main>
+  </div>
+)
+
+export default Layout
