84a47e95a80ac7e5e309bd9f3c3c91d794339fbd
Paul Craig <paul_craig_16@hotmail.com>
2018-04-15 23:18:22 -0400

    Add tests for TextFieldAdapter component

--- src/forms/__tests__/TextInput.test.js
+++ src/forms/__tests__/TextInput.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import { TextField } from '../TextInput'
+import { render, shallow } from 'enzyme'
+import { TextField, TextFieldAdapter } from '../TextInput'
 
 const defaultProps = {
   children: <label>Name</label>,
@@ -8,18 +8,38 @@ const defaultProps = {
   name: 'name',
 }
 
+const defaultAdapterProps = {
+  input: {},
+  ...defaultProps,
+}
+
+const getAdapterAttrib = (adapter, attr) =>
+  adapter.find('input')[0].attribs[attr]
+
 describe('<TextField> component', () => {
   it('has props assigned correctly', () => {
     const textField = shallow(<TextField {...defaultProps} />)
-    const textFieldInput = textField.find('input')
-    expect(textFieldInput).toBeTruthy()
-    expect(textFieldInput.props().type).toEqual('text')
-    expect(textFieldInput.props().id).toEqual('id')
-    expect(textFieldInput.props().name).toEqual('name')
+    const tfInput = textField.find('input')
+    expect(tfInput).toBeTruthy()
+    expect(tfInput.props().type).toEqual('text')
+    expect(tfInput.props().id).toEqual('id')
+    expect(tfInput.props().name).toEqual('name')
   })
 
   it('renders label correctly', () => {
     const textField = shallow(<TextField {...defaultProps} />)
     expect(textField.find('label').text()).toMatch(/Name/)
+  })
+})
+
+describe('<TextFieldAdapter> component', () => {
+  it('has props assigned directly through input object', () => {
+    const tfAdapter = render(<TextFieldAdapter {...defaultAdapterProps} />)
+    expect(tfAdapter.find('input')).toBeTruthy()
+
+    const tfInput = shallow(<TextField {...defaultProps} />).find('input')
+    expect(getAdapterAttrib(tfAdapter, 'type')).toEqual(tfInput.props().type)
+    expect(getAdapterAttrib(tfAdapter, 'id')).toEqual(tfInput.props().id)
+    expect(getAdapterAttrib(tfAdapter, 'name')).toEqual(tfInput.props().name)
   })
 })
