d588e0dbb8a13a5efd47a0ea80e02934b047ab23
Paul Craig <paul_craig_16@hotmail.com>
2018-04-16 16:21:30 -0400

    Add Layout component to pages
    Cuts down on a lot of boilerplate code for us.
    
    - Removed ErrorContent because it's not substantially different
      from our Content

--- src/CalendarPage.js
+++ src/CalendarPage.js
@@ -1,65 +1,44 @@
 import React, { Component } from 'react'
 import { Trans } from 'lingui-react'
 import { NavLink } from 'react-router-dom'
-import { H1, Content, CalHeader, CalReminder, Bold } from './styles'
-import PageHeader from './PageHeader'
-import AlphaBanner from './AlphaBanner'
-import FederalBanner from './FederalBanner'
-import Footer from './Footer'
+import { CalHeader, CalReminder, Bold } from './styles'
+import Layout from './Layout'
 import Button from './forms/Button'
 import Calendar from './Calendar'
 
 class CalendarPage extends Component {
   render() {
     return (
-      <div>
-        <AlphaBanner>
-          {' '}
-          <span>
-            <Trans>This is a new service we are constantly improving.</Trans>
-          </span>{' '}
-        </AlphaBanner>
-        <FederalBanner />
-        <main role="main">
-          <PageHeader>
-            <H1>
-              <Trans>Request a new Canadian Citizenship test date</Trans>
-            </H1>
-          </PageHeader>
-          <Content>
-            <NavLink to="/">
-              <p>
-                <Trans>← Go Back</Trans>
-              </p>
-            </NavLink>
+      <Layout>
+        <NavLink to="/">
+          <Trans>← Go Back</Trans>
+        </NavLink>
+        {/* TODO: add bottom spacing to Go Back links */}
+        <br />
+        <br />
 
-            <CalHeader>
-              <Trans>
-                Citizenship Tests are scheduled on <Bold>Tuesdays</Bold> and{' '}
-                <Bold>Fridays</Bold>. Use the calendar to select{' '}
-                <Bold>at least four days you’re available</Bold> in June and
-                July.
-              </Trans>
-            </CalHeader>
-            <Calendar />
-            <CalReminder>
-              <Trans>
-                Remember: make sure to stay available on all of the days you
-                select
-              </Trans>
-            </CalReminder>
-            <NavLink to="/confirmation">
-              <Button>
-                <Trans>Review →</Trans>
-              </Button>
-            </NavLink>
-            <NavLink to="/">
-              <Trans>Cancel</Trans>
-            </NavLink>
-          </Content>
-          <Footer topBarBackground="black" />
-        </main>
-      </div>
+        <CalHeader>
+          <Trans>
+            Citizenship Tests are scheduled on <Bold>Tuesdays</Bold> and{' '}
+            <Bold>Fridays</Bold>. Use the calendar to select{' '}
+            <Bold>at least four days you’re available</Bold> in June and July.
+          </Trans>
+        </CalHeader>
+        <Calendar />
+        <CalReminder>
+          <Trans>
+            Remember: make sure to stay available on all of the days you select
+          </Trans>
+        </CalReminder>
+        <NavLink to="/confirmation">
+          <Button>
+            <Trans>Review →</Trans>
+          </Button>
+        </NavLink>
+        <NavLink to="/">
+          <Trans>Cancel</Trans>
+        </NavLink>
+      </Layout>
     )
   }
 }
