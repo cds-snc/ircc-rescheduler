3d20e06670b19d33a28f9a0e013f66de3509df2f
Paul Craig <paul_craig_16@hotmail.com>
2018-05-04 00:08:11 -0400

    Create _convertDateToUTC function that prints the right days
    Unbelievably, we need to do this to get javascript's Date string functions
      to reliably print out the days that we've selected on the calendar.
      When dates are selected, a timezone is automatically created for the date,
      based on the Locale.
      Once you print the date, it will apply the timezone transformation and
      possibly return a different date as a string representation.
    
      For example, you might get:
      var d = new Date("2018-06-05").toString()
      Mon Jun 04 2018 20:00:00 GMT-0400 (EDT)
    
      So by converting to UTC time, we offset the local timezone and can reliably
      call string methods on our Date object. Amazing.
    
      eg:
      var d = _convertDateToUTC(new Date("2018-06-05")).toString()
      Tue Jun 05 2018 00:00:00 GMT-0400 (EDT)
    
      source: https://stackoverflow.com/a/14006555/9728185

--- web/src/Time.js
+++ web/src/Time.js
@@ -7,13 +7,46 @@ import PropTypes from 'prop-types'
 */
 const _isString = v => typeof v === 'string' || v instanceof String
 
+/*
+  Unbelievably, we need to do this to get javascript's Date string functions
+  to reliably print out the days that we've selected on the calendar.
+  When dates are selected, a timezone is automatically created for the date,
+  based on the Locale.
+  Once you print the date, it will apply the timezone transformation and
+  possibly return a different date as a string representation.
+
+  For example, you might get:
+  var d = new Date("2018-06-05").toString()
+  Mon Jun 04 2018 20:00:00 GMT-0400 (EDT)
+
+  So by converting to UTC time, we offset the local timezone and can reliably
+  call string methods on our Date object. Amazing.
+
+  eg:
+  var d = _convertDateToUTC(new Date("2018-06-05")).toString()
+  Tue Jun 05 2018 00:00:00 GMT-0400 (EDT)
+
+  source: https://stackoverflow.com/a/14006555/9728185
+*/
+const _convertDateToUTC = date => {
+  return new Date(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds(),
+  )
+}
+
 const dateToHTMLString = date => {
-  date = _isString(date) ? new Date(date) : date
+  date = _isString(date) ? _convertDateToUTC(new Date(date)) : date
+
   return date.toDateString()
 }
 
 const dateToISODateString = date => {
-  date = _isString(date) ? new Date(date) : date
+  date = _isString(date) ? _convertDateToUTC(new Date(date)) : date
 
   /*
   YYYY-MM-DD from "a valid date string"
