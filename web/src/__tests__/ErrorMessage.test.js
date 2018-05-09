6d97fb024c023b1ff176567efe799b8e52065b2e
Paul Craig <paul_craig_16@hotmail.com>
2018-05-09 18:25:07 -0400

    ErrorMessage accepts a React element or a string
    This way we can pass it <Trans> elemments so that our error
    messages work in English and French.

--- web/src/__tests__/ErrorMessage.test.js
+++ web/src/__tests__/ErrorMessage.test.js
@@ -10,12 +10,21 @@ describe('<ErrorMessage />', () => {
     expect(wrapper.props().id).toBe(undefined)
   })
 
-  it('renders with a message and an id', () => {
+  it('renders with a string message and an id', () => {
     const wrapper = shallow(
       <ErrorMessage message="Not one step backwards!" id="order-227" />,
     )
     expect(wrapper.find('span').length).toBe(1)
     expect(wrapper.find('span').text()).toEqual('Not one step backwards!')
+    expect(wrapper.props().id).toEqual('order-227')
+  })
+
+  it('renders with a react element message and an id', () => {
+    const wrapper = shallow(
+      <ErrorMessage message={<p>Not one step backwards!</p>} id="order-227" />,
+    )
+    expect(wrapper.find('span p').length).toBe(1)
+    expect(wrapper.find('span p').text()).toEqual('Not one step backwards!')
     expect(wrapper.props().id).toEqual('order-227')
   })
 })
