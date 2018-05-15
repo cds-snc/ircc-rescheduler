f551e79207533619f16ecd8d7dd1ee0a1b3ce5d9
Paul Craig <paul_craig_16@hotmail.com>
2018-05-14 23:25:30 -0400

    Update to GOV.UK button styles

--- web/src/forms/Button.js
+++ web/src/forms/Button.js
@@ -4,18 +4,34 @@ import { theme, roundedEdges, mediaQuery, incrementColor } from '../styles'
 const Button = styled.button`
   font-size: ${theme.font.lg};
   font-weight: 500;
+  line-height: 2;
+
   color: ${theme.colour.white};
-  background-color: ${theme.colour.gray};
-  border: 5px solid transparent;
-  outline: 0;
-  padding: ${theme.spacing.xs} ${theme.spacing.lg};
+  background-color: ${theme.colour.green};
+  border: none;
   cursor: pointer;
+
+  // Size and shape
+  position: relative;
+  display: inline-block;
+  padding: ${theme.spacing.sm} ${theme.spacing.lg};
   ${roundedEdges};
+
+  outline: 1px solid transparent; // keep some button appearance when changing colour settings in browsers
+  outline-offset: -1px; // fixes bug in Safari that outline width on focus is not overwritten, is reset to 0 on focus in govuk_template
+  -webkit-appearance: none;
+
+  // Bottom edge effect
+  box-shadow: 0 2px 0 ${incrementColor(theme.colour.grey, 80)};
 
   ${mediaQuery.small(css`
     width: 100%;
     padding: ${theme.spacing.sm} ${theme.spacing.lg};
   `)};
+
+  &:visited {
+    background-color: ${theme.colour.green};
+  }
 
   &:focus {
     outline: 4px solid ${theme.colour.focus};
@@ -23,9 +39,15 @@ const Button = styled.button`
   }
 
   &:hover,
-  &:active,
   &:focus {
-    background-color: ${incrementColor(theme.colour.gray, 20)};
+    background-color: ${incrementColor(theme.colour.green, 20)};
+    box-shadow: 0 2px 0 ${incrementColor(theme.colour.grey, 100)};
+  }
+
+  &:active {
+    top: 2px;
+    background-color: ${incrementColor(theme.colour.green, 20)};
+    box-shadow: 0 0 0 ${incrementColor(theme.colour.green, 20)};
   }
 
   &:active,
@@ -37,8 +59,27 @@ const Button = styled.button`
   &:disabled {
     &:hover {
       cursor: not-allowed;
-      background-color: ${incrementColor(theme.colour.gray, 30)};
+      background-color: ${incrementColor(theme.colour.green, 30)};
     }
+  }
+
+  // making the click target bigger than the button
+  // (and fill the space made when the button moves)
+  &:before {
+    content: '';
+    height: 110%;
+    width: 100%;
+    display: block;
+    background: transparent;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  &:hover:before,
+  &:active:before {
+    top: -10%;
+    height: 120%;
   }
 `
 
