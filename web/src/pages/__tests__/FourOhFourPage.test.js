6158a6205c3379c4d9691cc613c4c225a924562e
Paul Craig <paul_craig_16@hotmail.com>
2018-05-15 22:51:13 -0400

    Add rudimentary tests for ErrorPage and 404 Page

---
+++ web/src/pages/__tests__/FourOhFourPage.test.js
@@ -0,0 +1,12 @@
+import { shallow, mount } from 'enzyme'
+import FourOhFourPage from '../FourOhFourPage'
+import React from 'react'
+
+describe('<FourOhFourPage />', () => {
+  it('can be instantiated', () => {
+    const wrapper = shallow(<FourOhFourPage />)
+    // have to use mount so that we don't get <withI18N />
+    const header = mount(wrapper.props().children[0])
+    expect(header.text()).toEqual('404: Page not found')
+  })
+})
