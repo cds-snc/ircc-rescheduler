cc3690f397b433d7891173bdae02c90aaf2dfa7e
Paul Craig <paul_craig_16@hotmail.com>
2018-05-18 17:57:16 -0400

    Add tests for ValidationMessage
    Since mostly this is the same as ErrorMessage I've created an array
    so we can map over our components.
    
    - this means we have to use React.createElement to render the
      component in each test
    - the last test checking for alert-related attributes is the
      only test that differs between these components
      - better practice is probably not to include logic in tests
        but when I'm being judged for my sins at the end of my life
        I don't think this will be the one we get hung up on

--- web/src/components/__tests__/ErrorMessage.test.js
+++ web/src/components/__tests__/ErrorMessage.test.js
@@ -1,28 +1,65 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import ErrorMessage from '../ErrorMessage'
+import ErrorMessage, { ValidationMessage } from '../ErrorMessage'
 
-describe('<ErrorMessage />', () => {
-  it('renders without a message or id', () => {
-    const wrapper = shallow(<ErrorMessage />)
-    expect(wrapper.find('span').length).toBe(1)
-    expect(wrapper.find('span').text()).toEqual('')
-    expect(wrapper.props().id).toBe(undefined)
-  })
+const messages = [
+  {
+    component: ValidationMessage,
+    toString: '<ValidationMessage />',
+    alert: false,
+  },
+  {
+    component: ErrorMessage,
+    toString: '<ErrorMessage />',
+    alert: true,
+  },
+]
+messages.map(({ component, alert, toString }) => {
+  describe(`${toString}`, () => {
+    it('renders without a message or id', () => {
+      const wrapper = shallow(React.createElement(component))
+      expect(wrapper.find('span').length).toBe(1)
+      expect(wrapper.find('span').text()).toEqual('')
+      expect(wrapper.props().id).toBe(undefined)
+    })
 
-  it('renders with a string message and an id', () => {
-    const wrapper = shallow(
-      <ErrorMessage message="Not one step backwards!" id="order-227" />,
-    )
-    expect(wrapper.find('span').text()).toEqual('Not one step backwards!')
-    expect(wrapper.props().id).toEqual('order-227')
-  })
+    it('renders with a string message and an id', () => {
+      const wrapper = shallow(
+        React.createElement(component, {
+          message: 'Not one step backwards!',
+          id: 'order-227',
+        }),
+      )
+      expect(wrapper.find('span').text()).toEqual('Not one step backwards!')
+      expect(wrapper.props().id).toEqual('order-227')
+    })
 
-  it('renders with a react element message and an id', () => {
-    const wrapper = shallow(
-      <ErrorMessage message={<p>Not one step backwards!</p>} id="order-227" />,
-    )
-    expect(wrapper.find('span p').text()).toEqual('Not one step backwards!')
-    expect(wrapper.props().id).toEqual('order-227')
+    it('renders with a react element message and an id', () => {
+      const wrapper = shallow(
+        React.createElement(component, {
+          message: React.createElement('p', null, 'Not one step backwards!'),
+          id: 'order-227',
+        }),
+      )
+      expect(wrapper.find('span p').text()).toEqual('Not one step backwards!')
+      expect(wrapper.props().id).toEqual('order-227')
+    })
+
+    it(`renders ${alert ? 'with' : 'without'} alert attributes`, () => {
+      const wrapper = shallow(
+        React.createElement(component, {
+          message: 'Not one step backwards!',
+          id: 'order-227',
+        }),
+      )
+      expect(wrapper.find('span').text()).toEqual('Not one step backwards!')
+      if (alert) {
+        expect(wrapper.props()).toHaveProperty('role', 'alert')
+        expect(wrapper.props()).toHaveProperty('aria-live', 'assertive')
+      } else {
+        expect(wrapper.props()).not.toHaveProperty('role')
+        expect(wrapper.props()).not.toHaveProperty('aria-live')
+      }
+    })
   })
 })
