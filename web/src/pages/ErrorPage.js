17cd35fa398ec11e6f4b31f89a637b0b5dabf80b
Paul Craig <paul_craig_16@hotmail.com>
2018-05-18 00:25:23 -0400

    Add Contact component
    We're putting IRCC contact information in a few places and
    it should always be the same.
    
    Therefore, it seems fair enough to componentize it.

--- web/src/pages/ErrorPage.js
+++ web/src/pages/ErrorPage.js
@@ -1,32 +1,30 @@
 import React from 'react'
 import { Trans } from 'lingui-react'
-import { css } from 'react-emotion'
 import { NavLink } from 'react-router-dom'
-import { H1, theme } from '../styles'
+import { H1, H2 } from '../styles'
 import Layout from '../components/Layout'
-
-const contentClass = css`
-  p {
-    padding-bottom: ${theme.spacing.lg};
-  }
-`
+import Contact from '../components/Contact'
 
 class ErrorPage extends React.Component {
   render() {
     return (
-      <Layout contentClass={contentClass}>
+      <Layout>
         <H1>
           <Trans>Request failed</Trans>
         </H1>
-        <p>
-          <Trans>
-            Please contact IRCC directly in order to change your appointment.
-          </Trans>
-        </p>
-        <p>vancouverIRCC@cic.gc.ca</p>
-        <p>1-888-242-2100</p>
+        <Contact>
+          <H2>
+            <Trans>
+              Please contact{' '}
+              <abbr title="Immigration, Refugees and Citizenship Canada">
+                IRCC
+              </abbr>{' '}
+              directly to reschedule your appointment
+            </Trans>
+          </H2>
+        </Contact>
         <NavLink to="/">
-          <Trans>← Go Back</Trans>
+          ← <Trans>Home</Trans>
         </NavLink>
       </Layout>
     )
