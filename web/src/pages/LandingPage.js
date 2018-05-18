4f1a495f0bcebe0e6e29a6eeccda2d2ebeb66128
Paul Craig <paul_craig_16@hotmail.com>
2018-05-18 12:01:28 -0400

    Gut Reminder component but use it more
    Our Reminder CSS has largely been set on Content at this point,
    so I deleted almost all of it.
    
    Having said that, it's a useful placeholder for us because we
    have some Reminder-styled stuff in our mockups that we need to
    sort out eventually.

--- web/src/pages/LandingPage.js
+++ web/src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import styled, { css } from 'react-emotion'
 import { NavLink } from 'react-router-dom'
 import { H1, H2, H3, theme } from '../styles'
 import Layout from '../components/Layout'
+import Reminder from '../components/Reminder'
 import { buttonStyles } from '../components/forms/Button'
 import { Trans } from 'lingui-react'
 import { Helmet } from 'react-helmet'
@@ -104,7 +105,7 @@ class LandingPage extends React.Component {
           </p>
         </section>
 
-        <section>
+        <Reminder>
           <H3>
             <Trans>
               Important: by sending this request to reschedule, you will be
@@ -113,7 +114,7 @@ class LandingPage extends React.Component {
               date.
             </Trans>
           </H3>
-        </section>
+        </Reminder>
 
         <NavLink to="/register" className={buttonStyles}>
           <Trans>Start Now</Trans> →
