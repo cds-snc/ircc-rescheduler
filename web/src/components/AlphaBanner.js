e707c50213f51db2863a0684f7cb1ae258033485
nmakuch <makuch.nick@gmail.com>
2018-06-12 18:24:53 -0400

    Alpha Banner Alignment

--- web/src/components/AlphaBanner.js
+++ web/src/components/AlphaBanner.js
@@ -5,10 +5,26 @@ import { PhaseBanner } from '@cdssnc/gcui'
 let AlphaBanner = styled(PhaseBanner)`
   padding-top: ${theme.spacing.sm};
   padding-bottom: ${theme.spacing.sm};
+  display: flex;
+
+  span:last-of-type {
+    margin-left: 0;
+    ${mediaQuery.sm(css`
+      padding: 0;
+      line-height: 1.3;
+    `)};
+  }
+
+  span:first-of-type {
+    padding: 0 ${theme.spacing.md} 0 ${theme.spacing.md};
+    ${mediaQuery.sm(css`
+      padding: 0.1rem ${theme.spacing.md} 0.1rem ${theme.spacing.md};
+    `)};
+  }
 
   ${mediaQuery.sm(css`
-    padding-left: ${theme.spacing.xl};
-    padding-bottom: ${theme.spacing.sm};
+    padding: ${theme.spacing.md} 0 ${theme.spacing.md} ${theme.spacing.xl};
+    display: flex;
   `)};
 `
 
