39a90082ee7c75cd4b79d082507f208b264c45d9
nmakuch <makuch.nick@gmail.com>
2018-06-13 19:36:30 -0400

    Code Review Changes

--- web/src/components/Reminder.js
+++ web/src/components/Reminder.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { css } from 'react-emotion'
-import { theme, mediaQuery } from '../styles'
+import { theme } from '../styles'
 import importantMessage from '../assets/importantMessage.svg'
 
 const imBanner = css`
@@ -13,10 +13,6 @@ const imBanner = css`
   padding: ${theme.spacing.lg} ${theme.spacing.xl} ${theme.spacing.lg}
     ${theme.spacing.xl};
 
-  ${mediaQuery.md(css`
-    display: block;
-  `)};
-
   span {
     font-size: ${theme.font.md};
     display: block;
@@ -27,22 +23,17 @@ const icon = css`
   height: 3rem;
   display: block;
   margin-right: ${theme.spacing.lg};
-
-  ${mediaQuery.md(css`
-    float: left;
-    margin-top: ${theme.spacing.xs};
-    margin-right: ${theme.spacing.md};
-  `)};
 `
 
-const Reminder = ({ children }) => (
-  <div className={imBanner}>
+const Reminder = ({ children, className = '' }) => (
+  <div className={`${imBanner} ${className}`}>
     <img src={importantMessage} className={icon} alt="Important Message" />
     <span>{children}</span>
   </div>
 )
 Reminder.propTypes = {
   children: PropTypes.any.isRequired,
+  className: PropTypes.string,
 }
 
 export default Reminder
