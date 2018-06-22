c1cb9947cccd0c1df143ff4a08f743fb08a50f9b
Dave Samojlenko <dsamojlenko@gmail.com>
2018-06-22 13:54:19 -0400

    add shared files

--- shipitfile.js
+++ shipitfile.js
@@ -2,17 +2,22 @@ module.exports = shipit => {
   // Load shipit-deploy tasks
   require('shipit-deploy')(shipit)
   require('shipit-npm')(shipit);
+  require('shipit-shared')(shipit);
 
   shipit.initConfig({
     default: {
-      branch: 'master',
+      branch: 'deploy_with_pm2',
       deployTo: '/home/ircc/ircc-rescheduler',
       repositoryUrl: 'https://github.com/cds-snc/ircc-rescheduler.git',
+      shared: {
+        overwrite: true,
+        files: [
+          'api/.env',
+        ],
+      }
     },
     dev: {
       servers: 'ircc@ircc-vm-2.canadacentral.cloudapp.azure.com',
     },
-  })
-
-  shipit.remote('ls -al');
+  });
 }
