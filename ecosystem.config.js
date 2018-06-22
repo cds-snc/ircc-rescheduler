a1ba96951d448ec35de7ad430fee4a88a9be396d
Dave Samojlenko <dsamojlenko@gmail.com>
2018-06-22 10:29:21 -0400

    remove pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,20 +13,5 @@ module.exports = {
       script     : "server.js",
       instances  : "max",
       exec_mode  : "cluster"
-    }],
-    "deploy": {
-        "production": {
-            "user": "ircc",
-            "host": ["13.71.191.141"],
-            "ref": "origin/deploy_with_pm2",
-            "repo": "https://github.com/cds-snc/ircc-rescheduler.git",
-            "path": "/home/ircc/ircc-rescheduler-new",
-            "ssh_options": "StrictHostKeyChecking=no",
-            "post-setup": "ls -al",
-            "post-deploy": "yarn restart",
-            "env": {
-                "NODE_ENV": "production"
-            }
-        }
-    }
+    }]
   }
