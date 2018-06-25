054ac26d6104d06848e039a500028b3daea35928
Amir <amir.abou-jawde@cds-snc.ca>
2018-06-25 11:08:55 -0400

    Get the links on the footer to start translating
    created a new method called translateText, was having issues with using the conventional way of getting the i18n text via i18n._`text` with regards to hyperlinks. Now uses i18n._('string') which resolves this.

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { WordMark } from '@cdssnc/gcui'
 import styled, { css } from 'react-emotion'
+import { Trans, withI18n } from 'lingui-react'
 import { theme, mediaQuery, visuallyhiddenMobile } from '../styles'
 
 const Circle = styled.span`
@@ -89,7 +90,17 @@ const TopBar = styled.hr(
   props => ({ background: props.background }),
 )
 
-const Footer = ({ topBarBackground }) => (
+/*
+  Helper method that checks to see if the i18n object is null or not, since it ends up being null 
+  if the user refreshes the page. Returns normal english (i18n.t doesnt work and returns english) otherwise.
+  The reason we have to sometimes use this method over say the <Trans> tag is that we need the text to be a string at compile time (it's a React object at compile time)
+  */
+const translateText = (i18n, text) => {
+  const translation = i18n === undefined ? text : i18n._(text)
+  return translation
+}
+
+const Footer = ({ topBarBackground, i18n }) => (
   <div>
     {topBarBackground ? <TopBar background={topBarBackground} /> : ''}
     <footer className={footer}>
@@ -103,19 +114,33 @@ const Footer = ({ topBarBackground }) => (
       </div>
 
       <div className={bottomLinks}>
-        <a href="mailto:cds-snc@tbs-sct.gc.ca">Contact</a>
+        <a href="mailto:cds-snc@tbs-sct.gc.ca">
+          <Trans>Contact</Trans>
+        </a>
         <Circle>&#9679;</Circle>
-        <a href="https://www.canada.ca/en/transparency/privacy.html">Privacy</a>
+        <a
+          href={translateText(
+            i18n,
+            'https://www.canada.ca/en/transparency/privacy.html',
+          )}
+        >
+          <Trans>Privacy</Trans>
+        </a>
         <Circle>&#9679;</Circle>
-        <a href="https://digital.canada.ca/legal/terms/">
-          Terms<span className={visuallyhiddenMobile}> and Conditions</span>
+        <a href={translateText(i18n, 'https://digital.canada.ca/legal/terms/')}>
+          <Trans>Terms</Trans>
+          <span className={visuallyhiddenMobile}>
+            {' '}
+            <Trans>and Conditions</Trans>
+          </span>
         </a>
       </div>
     </footer>
   </div>
 )
 Footer.propTypes = {
+  i18n: PropTypes.object,
   topBarBackground: PropTypes.string,
 }
 
-export default Footer
+export default withI18n()(Footer)
