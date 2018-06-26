4a36ed6e3ece3f62e1af3fddcde2871c29192267
nmakuch <makuch.nick@gmail.com>
2018-06-26 18:02:00 -0400

    headerClass ? skinnyBanner : bigBanner

--- web/src/components/PageHeader.js
+++ web/src/components/PageHeader.js
@@ -5,7 +5,7 @@ import { theme, mediaQuery } from '../styles'
 import { Trans } from 'lingui-react'
 import PhaseBanner from './PhaseBanner'
 
-const banner = css`
+const bigBanner = css`
   background-color: ${theme.colour.blue};
   color: ${theme.colour.white};
   padding: ${theme.spacing.lg} ${theme.spacing.xxxl};
@@ -25,8 +25,23 @@ const banner = css`
   }
 `
 
+const skinnyBanner = css`
+  background-color: ${theme.colour.blue};
+  color: ${theme.colour.white};
+  padding: ${theme.spacing.sm} ${theme.spacing.xxxl} 0.55rem
+    ${theme.spacing.xxxl};
+
+  ${mediaQuery.sm(css`
+    padding: ${theme.spacing.sm} ${theme.spacing.xl} 0.55rem ${theme.spacing.xl};
+  `)};
+
+  div {
+    margin-bottom: 0;
+  }
+`
+
 const PageHeader = ({ children, headerClass = '' }) => (
-  <header className={banner}>
+  <header className={headerClass ? skinnyBanner : bigBanner}>
     <PhaseBanner phase="beta" color={`${theme.colour.white}`}>
       <Trans>
         This is a new service, help us improve by{' '}
