479c9924f6a83060a59db9fb9a5e7793bfb492fe
nmakuch <makuch.nick@gmail.com>
2018-06-25 20:18:13 -0400

    text color switching

--- web/src/components/AlphaBanner.js
+++ web/src/components/AlphaBanner.js
@@ -5,10 +5,19 @@ import { PhaseBanner } from '@cdssnc/gcui'
 
 const beta = css`
   background-color: #22a7f0;
+  border-radius: 5px;
+  padding: ${theme.spacing.xxs} ${theme.spacing.xs};
+  font-size: ${theme.font.xs};
+  color: ${theme.colour.white};
 `
 
 const alpha = css`
   background-color: red;
+`
+
+const message = css`
+  font-size: ${theme.font.xs};
+  margin-left: ${theme.spacing.md};
 `
 
 class AlphaBanner extends React.Component {
@@ -18,7 +27,9 @@ class AlphaBanner extends React.Component {
         {this.props.phase === 'beta' ? (
           <div>
             <span className={beta}>BETA</span>
-            <span>{this.props.children}</span>
+            <span className={message} style={{ color: this.props.color }}>
+              {this.props.children}
+            </span>
           </div>
         ) : (
           <div>
