4a199e49a1c2e1129d0bf1b5dc80fd3e41042a7e
Amir <amir.abou-jawde@cds-snc.ca>
2018-06-28 18:06:56 -0400

    Add security headers and tests for them. As well as helmet api.
    Also updated yarn.lock file, so probably lots of our deps got
    patch upgrades.

--- web/test/server.test.js
+++ web/test/server.test.js
@@ -30,6 +30,25 @@ describe('Server Side Rendering', () => {
 
   it('renders a reassuring confirmation message at /confirmation', async () => {
     let response = await request(server).get('/confirmation')
-    expect(response.text).toMatch(/Thank you! Your request has been received./)
+    expect(response.text).toMatch('Thank you! Your request has been received.')
+  })
+
+  it('has xss protection', async () => {
+    let response = await request(server).get('/')
+    expect(response.header['x-xss-protection']).toEqual('1; mode=block')
+  })
+
+  it('has nosniff enabled', async () => {
+    let response = await request(server).get('/')
+    expect(response.header['x-content-type-options']).toEqual('nosniff')
+  })
+
+  it('has frame protection', async () => {
+    let response = await request(server).get('/')
+    expect(response.header['x-frame-options']).toEqual('DENY')
+  })
+  it('has no-store protection', async () => {
+    let response = await request(server).get('/')
+    expect(response.header['surrogate-control']).toEqual('no-store')
   })
 })
