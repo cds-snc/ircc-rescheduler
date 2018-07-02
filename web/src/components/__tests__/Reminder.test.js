e466036d867d2a3b1599989eb253a1c94a8ef340
Paul Craig <paul_craig_16@hotmail.com>
2018-07-02 17:45:42 -0400

    Translate alt-text in Reminder component

--- web/src/components/__tests__/Reminder.test.js
+++ web/src/components/__tests__/Reminder.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import Reminder from '../Reminder'
 
 describe('<Reminder />', () => {
   it('renders reminder message', () => {
-    const reminder = shallow(<Reminder>Message</Reminder>)
+    const reminder = mount(<Reminder>Message</Reminder>)
     expect(reminder.find('img').length).toBe(1)
     expect(reminder.find('span').length).toBe(1)
   })
