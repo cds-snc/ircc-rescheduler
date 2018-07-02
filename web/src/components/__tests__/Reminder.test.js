125d552744e4adaafb8a8739865654fec9b6862c
Paul Craig <paul_craig_16@hotmail.com>
2018-07-02 17:56:21 -0400

    Add translateable page title
    I _knew_ there was a way!
    
    Updates immediately when JS is running.
    Otherwise, comes back from the server when a language has been set.

--- web/src/components/__tests__/Reminder.test.js
+++ web/src/components/__tests__/Reminder.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { render } from 'enzyme'
 import Reminder from '../Reminder'
 
 describe('<Reminder />', () => {
   it('renders reminder message', () => {
-    const reminder = mount(<Reminder>Message</Reminder>)
+    const reminder = render(<Reminder>Message</Reminder>)
     expect(reminder.find('img').length).toBe(1)
     expect(reminder.find('span').length).toBe(1)
   })
