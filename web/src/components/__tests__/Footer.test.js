22b49a1f355864711e8e02111b2ccbb393f7a665
Paul Craig <paul_craig_16@hotmail.com>
2018-07-02 17:02:49 -0400

    Translate footer links
    This is basically a cut + paste from @Amirroracle's pull request.
    
    It's great that this is working, but it's super gross.
    
    The only other way I could think to do this was very similar,
    passing an i18n object down through the context, but that seemed
    just as complicated.
    
    This split between in-page content and static text is fundamentally
    unhelpful.
    
    Ultimately, this might be a reason to give up lingui.
    
    (which is a shame, because other than this one problem it's really
    nice to use).

--- web/src/components/__tests__/Footer.test.js
+++ web/src/components/__tests__/Footer.test.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { shallow, render } from 'enzyme'
+import { mount } from 'enzyme'
 import { FooterBase as Footer } from '../Footer'
 import { getStore } from './LanguageSwitcher.test.js'
 
 describe('<Footer />', () => {
   it('renders footer', () => {
-    const footer = shallow(<Footer context={getStore('en')} />)
+    const footer = mount(<Footer context={getStore('en')} />)
     expect(footer.find('footer').length).toBe(1)
     expect(footer.find('hr').length).toBe(0)
   })
 
   it('renders footer with topBar', () => {
-    // have to use 'render' instead of 'shallow' to render nested components
-    const footer = render(
+    // have to use 'mount' instead of 'shallow' to render nested components
+    const footer = mount(
       <Footer topBarBackground="black" context={getStore('en')} />,
     )
     expect(footer.find('footer').length).toBe(1)
