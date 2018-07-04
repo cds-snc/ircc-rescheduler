190f8e13261aabe2cd426db7602068ae5a3a0752
Dave Samojlenko <dsamojlenko@gmail.com>
2018-07-04 00:10:56 -0400

    setup production deploy

--- shipitfile.js
+++ shipitfile.js
@@ -7,7 +7,6 @@ module.exports = shipit => {
   shipit.initConfig({
     default: {
       branch: 'master',
-      deployTo: '/home/ircc/ircc-rescheduler',
       repositoryUrl: 'https://github.com/cds-snc/ircc-rescheduler.git',
       shared: {
         overwrite: true,
@@ -19,7 +18,19 @@ module.exports = shipit => {
       },
     },
     dev: {
-      servers: 'ircc@ircc-vm-2.canadacentral.cloudapp.azure.com',
+      deployTo: '/home/ircc/ircc-rescheduler',
+      servers: 'ircc@ircc-vm-2.canadacentral.cloudapp.azure.com'
     },
+    production: {
+      branch: 'new_local_dev',
+      deployTo: '/home/ubuntu/ircc-rescheduler',
+      key: 'ircc-vm.pem',
+      servers: [
+        {
+          host: 'ec2-35-183-87-90.ca-central-1.compute.amazonaws.com',
+          user: 'ubuntu'
+        }
+      ]
+    }
   })
 }
