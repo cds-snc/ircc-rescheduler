bb19040d85b16ab000278f3e6ad06f9239107f05
Dave Samojlenko <dsamojlenko@gmail.com>
2018-07-03 22:36:29 -0400

    add production deploy info

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
       watch       : true,
       env: {
         "NODE_ENV": "development"
+      },
+      env_production: {
+        "NODE_ENV": "production"
       }
     },{
       name       : "web",
@@ -16,6 +19,37 @@ module.exports = {
       watch      : true,
       env: {
         "NODE_ENV": "development"
+      },
+      env_production: {
+        "NODE_ENV": "production"
       }
-    }]
+    }],
+    deploy: {
+      production: {
+        // SSH key path, default to $HOME/.ssh
+        key: "~/Downloads/ircc-vm.pem",
+        // SSH user
+        user: "ubuntu",
+        // SSH host
+        host: ["ec2-35-183-87-90.ca-central-1.compute.amazonaws.com"],
+        // SSH options with no command-line flag, see 'man ssh'
+        // can be either a single string or an array of strings
+        ssh_options: "StrictHostKeyChecking=no",
+        // GIT remote/branch
+        ref: "origin/new_local_dev",
+        // GIT remote
+        repo: "https://github.com/cds-snc/ircc-rescheduler.git",
+        // path in the server
+        path: "/home/ubuntu/ircc-rescheduler",
+        // Pre-setup command or path to a script on your local machine
+        // pre-setup: "apt-get install git ; ls -la",
+        // Post-setup commands or path to a script on the host machine
+        // eg: placing configurations in the shared dir etc
+        "post-setup": "ls -la",
+        // pre-deploy action
+        "pre-deploy-local": "echo 'This is a local executed command'",
+        // post-deploy action
+        "post-deploy": "yarn build_web && yarn build_api",
+      },
+    }
   }
