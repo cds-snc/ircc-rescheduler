f4b3d21ccb0821ddc121705032b935ab26c36cd2
Paul Craig <paul_craig_16@hotmail.com>
2018-07-04 11:55:36 -0400

    Pass in absolute uri for sever GraphQL call

--- web/src/utils/createApolloClient.js
+++ web/src/utils/createApolloClient.js
@@ -3,10 +3,10 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import fetch from 'isomorphic-fetch'
 
-const createApolloClient = ({ ssrMode }) =>
+const createApolloClient = ({ ssrMode, uri = '/graphql' }) =>
   new ApolloClient({
     ssrMode,
-    link: new HttpLink({ fetch }),
+    link: new HttpLink({ fetch, uri }),
     cache: new InMemoryCache(),
   })
 
