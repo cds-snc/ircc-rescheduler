aca9f1524312ba152e3ac44e7a715752523c156a
Dave Samojlenko <dsamojlenko@gmail.com>
2018-07-03 21:27:55 -0400

    move all env vars to env files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,13 +5,17 @@ module.exports = {
       script      : "index.js",
       watch       : true,
       env: {
-        "NODE_ENV": "production"
+        "NODE_ENV": "development"
       }
     },{
       name       : "web",
       cwd        : "./web/build",
       script     : "server.js",
       instances  : "max",
-      exec_mode  : "cluster"
+      exec_mode  : "cluster",
+      watch      : true,
+      env: {
+        "NODE_ENV": "development"
+      }
     }]
   }
