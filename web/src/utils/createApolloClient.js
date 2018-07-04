e2d6b7ea61e45d6a166e07a1215a9a573b53ea0e
Dave Samojlenko <dsamojlenko@gmail.com>
2018-07-03 21:31:27 -0400

    use relative url

--- web/src/utils/createApolloClient.js
+++ web/src/utils/createApolloClient.js
@@ -3,10 +3,7 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import fetch from 'isomorphic-fetch'
 
-const uri =
-  process.env.NODE_ENV === 'production'
-    ? 'https://rescheduler.cds-snc.ca/graphql'
-    : 'http://localhost:3004/graphql'
+const uri = '/graphql'
 
 const createApolloClient = ({ ssrMode }) =>
   new ApolloClient({
