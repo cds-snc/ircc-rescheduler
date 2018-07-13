bbefdc7461eb7bc0fb3817bad154a3e56849c5d8
Tim Arney <tim@line37.com>
2018-07-13 10:12:13 -0400

    date checks

--- web/src/__tests__/email.test.js
+++ web/src/__tests__/email.test.js
@@ -1,4 +1,5 @@
-import { sendMail } from '../email/sendmail'
+import { sendMail, cleanDates } from '../email/sendmail'
+import { humanReadable, datesMarkup } from '../email/email-template'
 const reqId = 'ad6b987e-4722-11e8-8771-5546b650d95e'
 const msgId = '01000162f3ba365d-b87e3e31-b93c-47bb-97ba-631c33ae7267-000000'
 
@@ -31,7 +32,7 @@ const options = {
   sendingAddress: 'receive-mock-sendmail@cds-snc.ca',
 }
 
-describe('Email', () => {
+describe('Send Email', () => {
   it('it handles response', async () => {
     const response = await sendMail(mockSES, options).catch(handleMailError)
     expect(response.messageId).toEqual(msgId)
@@ -42,5 +43,27 @@ describe('Email', () => {
     const response = await sendMail(mockSES, badOptions).catch(handleMailError)
     expect(response.errorMessage).toEqual('Invalid receivingAddress')
     expect(handleMailError.mock.calls.length).toBe(1)
+  })
+})
+
+describe('Handles Input Data for Email', () => {
+  it('it handles selected days', async () => {
+    const selectedDays = '2018-08-23,2018-08-30,2018-09-06'
+    let cleaned = cleanDates(selectedDays)
+    const dates = humanReadable(cleaned)
+    const formatted = datesMarkup(dates, '').trim()
+    expect(formatted).toEqual(
+      'Thursday August 23 2018 Thursday August 30 2018 Thursday September 06 2018',
+    )
+  })
+
+  it('Handles selected days with whitespace', async () => {
+    const selectedDays = '2018-08-23 ,2018-08-30, 2018-09-06 '
+    let cleaned = cleanDates(selectedDays)
+    const dates = humanReadable(cleaned)
+    const formatted = datesMarkup(dates, '').trim()
+    expect(formatted).toEqual(
+      'Thursday August 23 2018 Thursday August 30 2018 Thursday September 06 2018',
+    )
   })
 })
