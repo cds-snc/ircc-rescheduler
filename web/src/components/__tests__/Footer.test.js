0d3cc649de91e62b8273902d584dc1ce92defb79
Amir <amir.abou-jawde@cds-snc.ca>
2018-07-13 10:42:13 -0400

    change some tests to be more cohesive, footer no longer needs an i18n provider and reminder still needs one since I don't feel like adding withi18n for that component just for a test.

--- web/src/components/__tests__/Footer.test.js
+++ web/src/components/__tests__/Footer.test.js
@@ -3,15 +3,10 @@ import { mount, render } from 'enzyme'
 import { FooterBase as Footer } from '../Footer'
 import { getStore } from './LanguageSwitcher.test.js'
 import { i18n } from 'lingui-i18n/dist'
-import { I18nProvider } from 'lingui-react'
 
 describe('<Footer />', () => {
   it('renders footer', () => {
-    const footer = render(
-      <I18nProvider>
-        <Footer context={getStore('en')} i18n={i18n} />
-      </I18nProvider>,
-    )
+    const footer = render(<Footer context={getStore('en')} i18n={i18n} />)
     expect(footer.find('footer').length).toBe(1)
     expect(footer.find('hr').length).toBe(0)
   })
@@ -19,20 +14,14 @@ describe('<Footer />', () => {
   it('renders footer with topBar', () => {
     // have to use 'mount' instead of 'shallow' to render nested components
     const footer = mount(
-      <I18nProvider>
-        <Footer topBarBackground="black" context={getStore('en')} i18n={i18n} />
-      </I18nProvider>,
+      <Footer topBarBackground="black" context={getStore('en')} i18n={i18n} />,
     )
     expect(footer.find('footer').length).toBe(1)
     expect(footer.find('hr').length).toBe(1)
   })
 
   it('renders "and Conditions" in English', () => {
-    const footer = mount(
-      <I18nProvider>
-        <Footer context={getStore('en')} i18n={i18n} />
-      </I18nProvider>,
-    )
+    const footer = mount(<Footer context={getStore('en')} i18n={i18n} />)
     expect(
       footer
         .find('a')
@@ -42,11 +31,7 @@ describe('<Footer />', () => {
   })
 
   it('renders without "and Conditions" in French', () => {
-    const footer = mount(
-      <I18nProvider>
-        <Footer context={getStore('fr')} i18n={i18n} />
-      </I18nProvider>,
-    )
+    const footer = mount(<Footer context={getStore('fr')} i18n={i18n} />)
     expect(
       footer
         .find('a')
@@ -56,11 +41,7 @@ describe('<Footer />', () => {
   })
 
   it('renders with Canadawordmark in French with corresponding alt attr', () => {
-    const footer = mount(
-      <I18nProvider>
-        <Footer context={getStore('fr')} i18n={i18n} />{' '}
-      </I18nProvider>,
-    )
+    const footer = mount(<Footer context={getStore('fr')} i18n={i18n} />)
     expect(footer.find('img').length).toBe(1)
     expect(footer.find('img').prop('alt')).toEqual(
       'Symbole du gouvernement du Canada',
@@ -68,11 +49,7 @@ describe('<Footer />', () => {
   })
 
   it('renders with Canadawordmark in English with corresponding alt attr', () => {
-    const footer = mount(
-      <I18nProvider>
-        <Footer context={getStore('en')} i18n={i18n} />{' '}
-      </I18nProvider>,
-    )
+    const footer = mount(<Footer context={getStore('en')} i18n={i18n} />)
     expect(footer.find('img').length).toBe(1)
     expect(footer.find('img').prop('alt')).toEqual(
       'Symbol of the Government of Canada',
--- web/src/components/__tests__/Reminder.test.js
+++ web/src/components/__tests__/Reminder.test.js
@@ -3,15 +3,17 @@ import { render } from 'enzyme'
 import Reminder from '../Reminder'
 import { I18nProvider } from 'lingui-react'
 import { i18n } from 'lingui-i18n/dist'
+import { catalogs } from '../../utils/linguiUtils'
 
 describe('<Reminder />', () => {
   it('renders reminder message', () => {
     const reminder = render(
-      <I18nProvider>
+      <I18nProvider language={'fr'} catalogs={catalogs}>
         <Reminder i18n={i18n}>Message</Reminder>
       </I18nProvider>,
     )
     expect(reminder.find('img').length).toBe(1)
     expect(reminder.find('span').length).toBe(1)
+    expect(reminder.find('img').attr('alt')).toEqual('Message important')
   })
 })
