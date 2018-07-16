3357d0f181cf158f561999239545ed9dcd72c771
Amirroracle <amir.abou-jawde@cds-snc.ca>
2018-07-16 11:40:41 -0400

    Add bundle analyzer  (#252)
    *  webpack analyzer so that we can see how many resources are apis are taking
    
    * Amend changes -> add a reports directory for the analyzer and add it to git gitignore
    
    * add a prod line

--- web/razzle.config.js
+++ web/razzle.config.js
@@ -4,6 +4,20 @@ module.exports = {
     if (config.devServer) {
       config.devServer.host = '0.0.0.0'
     }
+    if (process.env.BUNDLE_CHECK) {
+      /* This allows us to analyze the the webpack bundle of all our apis and imports. 
+        You can change the analyzerMode to be 'server' and this may let you see more info like
+        what the files looked gzipped,parsed and standard etc.. */
+      const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+        .BundleAnalyzerPlugin
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          generateStatsFile: true,
+          reportFilename: '../../reports/report.html',
+        }),
+      )
+    }
     return config
   },
 }
