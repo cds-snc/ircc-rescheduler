3d1648a1b91a3b9cd928abea531fdd19eebbdf55
Amir <amir.abou-jawde@cds-snc.ca>
2018-07-16 11:19:36 -0400

    Amend changes -> add a reports directory for the analyzer and add it to git gitignore

--- web/razzle.config.js
+++ web/razzle.config.js
@@ -14,6 +14,7 @@ module.exports = {
         new BundleAnalyzerPlugin({
           analyzerMode: 'static',
           generateStatsFile: true,
+          reportFilename: '../../reports/report.html',
         }),
       )
     }
