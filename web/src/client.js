9c25f7e67138443fce314c379c5353192a6db7ea
Tim Arney <timarney@users.noreply.github.com>
2018-07-19 16:27:13 -0400

    Move Raven script load + install out of bundle to document (#278)
    * move Raven script load + install out of bundle to document
    
    * fix linting
    
    * add note about using dangerouslySetInnerHTML

--- web/src/client.js
+++ web/src/client.js
@@ -4,15 +4,6 @@ import { hydrate } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { ensureReady, After } from '@jaredpalmer/after'
 import routes from './routes'
-import { windowExists } from './utils/windowExists'
-
-import Raven from 'raven-js'
-if (windowExists() && process.env.NODE_ENV === 'production') {
-  window.Raven = Raven
-  window.Raven.config(
-    'https://a2315885b9c3429a918336c1324afa4a@sentry.io/1241616',
-  ).install()
-}
 
 ensureReady(routes).then(data =>
   hydrate(
