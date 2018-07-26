7927a96d49afa55fdcce6950818a6df71ca9b6b1
Tim Arney <tim@line37.com>
2018-07-26 09:52:22 -0400

    update run commands

--- web/cypress/integration/main/form.spec.js
+++ web/cypress/integration/main/form.spec.js
@@ -7,15 +7,15 @@ context('Actions', () => {
 
   it('should be able to fill in a profile', () => {
     cy.get('main a span').should('have.text', 'Start now')
-    cy.get('main a').click()
+    cy.get('main a').click({ force: true })
 
     cy.fixture('user').then(data => {
-      cy.get('#fullName').type(data.fullName)
-      cy.get('#email').type(data.email)
-      cy.get('#paperFileNumber').type(data.paperFileNumber)
-      cy.get('#reason-2').click()
-      cy.get('#explanation').type(data.explanation)
-      cy.get('#register-form').submit()
+      cy.get('#fullName').type(data.fullName, { force: true })
+      cy.get('#email').type(data.email, { force: true })
+      cy.get('#paperFileNumber').type(data.paperFileNumber, { force: true })
+      cy.get('#reason-2').click({ force: true })
+      cy.get('#explanation').type(data.explanation, { force: true })
+      cy.get('#register-form').submit({ force: true })
     })
 
     cy.url().should('contain', '/calendar')
@@ -24,20 +24,20 @@ context('Actions', () => {
       const count = el.length
 
       if (count < 3) {
-        cy.get('.DayPicker-NavButton--next').click()
+        cy.get('.DayPicker-NavButton--next').click({ force: true })
       }
 
       cy.get('.DayPicker-Day[aria-disabled=false]')
         .eq(0)
-        .click()
+        .click({ force: true })
       cy.get('.DayPicker-Day[aria-disabled=false]')
         .eq(1)
-        .click()
+        .click({ force: true })
       cy.get('.DayPicker-Day[aria-disabled=false]')
         .eq(2)
-        .click()
+        .click({ force: true })
 
-      cy.get('#cal-form').submit()
+      cy.get('#cal-form').submit({ force: true })
 
       cy.url().should('contain', '/review')
 
@@ -47,8 +47,8 @@ context('Actions', () => {
         cy.get('main').should('contain', data.explanation)
       })
 
-      cy.get('#review-form').submit()
-      
+      cy.get('#review-form').submit({ force: true })
+
       // should hit the confirm page now
       cy.url().should('contain', '/confirmation')
     })
