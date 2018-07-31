f5fdff15052fb07f5a0b71ecea9f0c9bdd8fe064
Tim Arney <tim@line37.com>
2018-07-31 12:19:13 -0400

    Update a11y tests to be part of CI process
    A11Y tests will now run as part of the CI process.  If page issues are found it will fail the build.

--- web/a11y.test.js
+++ web/a11y.test.js
@@ -1,10 +1,14 @@
 const pa11y = require('pa11y')
 const baseUrl = 'http://localhost:3004'
+
 const options = {
   log: {
     debug: console.log,
     error: console.error,
     info: console.log,
+  },
+  chromeLaunchConfig: {
+    args: ['--no-sandbox'], // pass sandbox flag for ci
   },
 }
 const registerActions = [
@@ -39,14 +43,23 @@ async function run() {
       }),
     )
 
+    let issues = []
     results.map(result => {
       if (result && result.issues && result.issues.length >= 1) {
         console.log(result)
+        issues.push(result)
       }
     })
+
+    if (issues.length >= 1) {
+      //process.exit(issues)
+      const count = issues.length
+      throw new Error(`Found ${count} page(s) with issues`)
+    }
   } catch (error) {
     // Output an error if it occurred
-    console.error(error.message)
+    console.error('\n\n' + error.message + '\n\n')
+    process.exit(1)
   }
 }
 
