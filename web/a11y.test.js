228f182b44c034ef3ed8d7dc3f1c179fe3d9618d
Tim Arney <timarney@users.noreply.github.com>
2018-08-01 11:34:41 -0400

    test auto-run a11y tests (#311)
    * Update a11y tests to be part of CI process
    
    A11Y tests will now run as part of the CI process.  If page issues are found it will fail the build.
    
    * Add ingore rule for Span.Name
    
    This seems to be a non issue for our app.  Case where this was failing is in the in Calendar where the next and back buttons are span elements with a role of button.  They do have an aria-label.
    
    Further information can be found here:
    
    https://www.w3.org/WAI/WCAG21/quickref/?versions=2.0#qr-ensure-compat-rsv
    
    If custom controls are created, however, or interface elements are programmed (in code or script) to have a different role and/or function than usual, then additional measures need to be taken to ensure that the controls provide important information to assistive technologies and allow themselves to be controlled by assistive technologies.

--- web/a11y.test.js
+++ web/a11y.test.js
@@ -1,11 +1,16 @@
 const pa11y = require('pa11y')
 const baseUrl = 'http://localhost:3004'
+
 const options = {
   log: {
     debug: console.log,
     error: console.error,
     info: console.log,
   },
+  chromeLaunchConfig: {
+    args: ['--no-sandbox'], // pass sandbox flag for ci
+  },
+  ignore: ['WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.Span.Name'],
 }
 const registerActions = [
   'set field #fullName to John Doe',
@@ -39,14 +44,23 @@ async function run() {
       }),
     )
 
+    let issues = []
     results.map(result => {
       if (result && result.issues && result.issues.length >= 1) {
         console.log(result)
+        issues.push(result)
       }
     })
+
+    if (issues.length >= 1) {
+      //process.exit(issues)
+      const count = issues.length
+      throw new Error(`Found ${count} page(s) with issues`)
+    }
   } catch (error) {
     // Output an error if it occurred
-    console.error(error.message)
+    console.error('\n\n' + error.message + '\n\n')
+    process.exit(1)
   }
 }
 
