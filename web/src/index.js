f5d79aeb1679963e052250cd6f89ced47704034d
Paul Craig <paul_craig_16@hotmail.com>
2018-08-07 16:41:32 -0400

    port number maybe maybe

--- web/src/index.js
+++ web/src/index.js
@@ -5,10 +5,21 @@ const server = http.createServer(app)
 
 let currentApp = app
 
+const { env } = require('process')
+
 /* eslint-disable no-console */
-console.log('PORT number', process.env.PORT)
-const port = process.env.PORT || 3000
-server.listen(port, error => {
+/* https://github.com/jaredpalmer/razzle/issues/356#issuecomment-366275253 */
+const port = () =>
+  parseInt(
+    env.RAZZLE_PORT ||
+      env.PORT ||
+      process.env.RAZZLE_PORT ||
+      process.env.PORT ||
+      3000,
+    10,
+  )
+
+server.listen(port(), error => {
   if (error) {
     console.log(error)
   }
