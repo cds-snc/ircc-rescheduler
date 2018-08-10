459f38338d2ebf8f66863e663d36d7d18cd842cd
Tim Arney <timarney@users.noreply.github.com>
2018-08-10 14:57:32 -0400

    French end-to-end test (#346)
    * French end-to-end test
    
    * Re-purpose full run English end-to-end test to handle French
    
    * test fix

---
+++ web/cypress/integration/main/form.spec-fr.js
@@ -0,0 +1,11 @@
+import { fullRun } from './full-run'
+
+context('Full Run-through', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should be able to fill in a profile and reach the confirmation page', () => {
+    fullRun(cy, 'fr')
+  })
+})
