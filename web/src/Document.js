ccaaf8bb5d8d14b21323cdf9bdede3d5a774ebab
Tim Arney <tim@line37.com>
2018-08-13 13:54:32 -0400

    Boost homepage load time using cache
    The loading times for slow 3G connections has been quite slow 30+secs.  This in part is due to the bundle size and parse time for the bundle.
    
    Given the the interactivity level and content for the initial landing page we can boost load time but serving up a cached version of the landing page and skip loading the client bundle for the initial page.

--- web/src/Document.js
+++ web/src/Document.js
@@ -85,12 +85,14 @@ class Document extends React.Component {
               />
             )}
 
-          <script
-            type="text/javascript"
-            src={assets.client.js}
-            defer
-            crossOrigin="anonymous"
-          />
+          {path !== '/' && (
+            <script
+              type="text/javascript"
+              src={assets.client.js}
+              defer
+              crossOrigin="anonymous"
+            />
+          )}
         </body>
       </html>
     )
