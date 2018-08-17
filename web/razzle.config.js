9493f8fe5ac419129f2928f7a27b85dd73dbccbd
Tim Arney <timarney@users.noreply.github.com>
2018-08-17 15:03:42 -0400

    Boost homepage load time using cache (#358)
    * Boost homepage load time using cache
    
    The loading times for slow 3G connections has been quite slow 30+secs.  This in part is due to the bundle size and parse time for the bundle.
    
    Given the the interactivity level and content for the initial landing page we can boost load time but serving up a cached version of the landing page and skip loading the client bundle for the initial page.

--- web/razzle.config.js
+++ web/razzle.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   modify: (config, { target, dev }, webpack) => {
+    
+    /* Ignore so we don't include these in the bundle */
+    config.plugins.push(
+      new webpack.IgnorePlugin(/^\.\/(?!en)(.+)$/, /validatorjs\/src\/lang/),
+    )
+
     if (process.env.CI) config.performance = { hints: false }
     if (config.devServer) {
       config.devServer.host = '0.0.0.0'
