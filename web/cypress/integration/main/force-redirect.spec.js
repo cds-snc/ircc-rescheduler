bbd88655d6a8ba500d54e59a2c2e36c2617499a3
Tim Arney <timarney@users.noreply.github.com>
2018-08-30 16:04:29 -0400

    Enforce subdomain (#414)
    * Enforce subdomain
    
    When a user is visiting the website using the staging or live url + the base sub-domain we need to redirect to a proper location sub-domain.
    
    Later we will 404 the use of the base sub-domain but this case needs to be handled for existing links.
    
    * fix to use protocol

---
+++ web/cypress/integration/main/force-redirect.spec.js
@@ -0,0 +1,39 @@
+context('Force redirect', () => {
+  it('should force redirect from "local" dev server', () => {
+    cy.visit('http://rescheduler-dev.vcap.me:3004')
+    cy.url().should('include', 'vancouver')
+  })
+
+  it('should force redirect from "local" live server', () => {
+    cy.visit('http://rescheduler-dev.vcap.me:3004')
+    cy.url().should('include', 'vancouver')
+  })
+
+  it('should force not redirect if on "not found" page', () => {
+    cy.visit('http://rescheduler-dev.vcap.me:3004/not-found')
+    cy.get('h1')
+      .eq(0)
+      .should('contain', 'Page not found.')
+  })
+
+  it('should force not redirect if not on 500 page', () => {
+    cy.visit('http://rescheduler-dev.vcap.me:3004/500')
+    cy.get('h1')
+      .eq(0)
+      .should('contain', 'Server Error.')
+  })
+
+  it('should force redirect from "local" live server with UTM', () => {
+    cy.visit(
+      'http://rescheduler.vcap.me:3004?utm_source=BELA%20email&utm_medium=email',
+    )
+    cy.url().should('include', 'vancouver')
+  })
+
+  it("should force redirect if sub-domain isn't whitelisted", () => {
+    cy.visit('http://test.rescheduler-dev.vcap.me:3004')
+    cy.get('h1')
+      .eq(0)
+      .should('contain', 'Page not found.')
+  })
+})
