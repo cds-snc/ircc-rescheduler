3be2ceeed0c3a3d4d4a26c52702564715f483700
Tim Arney <tim@line37.com>
2018-09-07 11:20:18 -0400

    translation

--- web/src/utils/flags.js
+++ web/src/utils/flags.js
@@ -3,12 +3,6 @@ export const parseFlags = () => {
   try {
     if (process.env.RAZZLE_FLAGS) {
       flags = JSON.parse(process.env.RAZZLE_FLAGS)
-      // eslint-disable-next-line no-console
-      console.log(
-        `%c FLAGS %c ${process.env.RAZZLE_FLAGS}  `,
-        'background-color: green; color: #fff; padding: 2px; font-weight: bold;',
-        'background-color: #efefef; padding: 2px;',
-      )
     }
   } catch (e) {
     // do nothing
