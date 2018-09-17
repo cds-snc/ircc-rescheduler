3b9010b40ccd9a7d704cb3006c8bb724321ef492
nmakuch <makuch.nick@gmail.com>
2018-09-17 17:36:18 -0400

    Updated Summary tests

--- web/src/components/__tests__/Summary.test.js
+++ web/src/components/__tests__/Summary.test.js
@@ -64,17 +64,21 @@ describe('<Summary />', () => {
     )
 
     const numOfSummaryRows = wrapper.find('SummaryRow')
+    const numOfTextSummaryRows = wrapper.find('TextAreaSummaryRow')
 
-    expect(numOfSummaryRows.length).toBe(6)
+    expect(numOfSummaryRows.length).toBe(5)
+    expect(numOfTextSummaryRows.length).toBe(1)
     expect(numOfSummaryRows.at(0).prop('summaryBody')).toEqual('Test1')
     expect(numOfSummaryRows.at(1).prop('summaryBody')).toEqual('test@test.com')
     expect(numOfSummaryRows.at(2).prop('summaryBody')).toEqual('12346789')
     expect(numOfSummaryRows.at(3).prop('summaryBody').props.id).toEqual(
       'Travel',
     )
-    expect(numOfSummaryRows.at(4).prop('summaryBody')).toEqual('feeling lazy')
+    expect(numOfTextSummaryRows.at(0).prop('summaryBody')).toEqual(
+      'feeling lazy',
+    )
     expect(
-      numOfSummaryRows.at(5).prop('summaryBody').props.selectedDays,
+      numOfSummaryRows.at(4).prop('summaryBody').props.selectedDays,
     ).toMatchObject(selectedDays)
   })
 })
