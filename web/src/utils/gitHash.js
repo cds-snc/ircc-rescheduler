3e6c6b3072b1a739c851d8dd2490a71d5ed13f91
Paul Craig <paul_craig_16@hotmail.com>
2018-09-21 13:18:57 -0400

    we know the file path

--- web/src/utils/gitHash.js
+++ web/src/utils/gitHash.js
@@ -18,31 +18,6 @@ export default () => {
     gitHashString = gitHashString.toString('utf8').replace(/\s/g, '') + '-1'
   } catch (e) {}
 
-  try {
-    gitHashString = readFileSync('../VERSION')
-    gitHashString = gitHashString.toString('utf8').replace(/\s/g, '') + '-2'
-  } catch (e) {}
-
-  try {
-    gitHashString = readFileSync('../../VERSION')
-    gitHashString = gitHashString.toString('utf8').replace(/\s/g, '') + '-3'
-  } catch (e) {}
-
-  try {
-    gitHashString = readFileSync('../../../VERSION')
-    gitHashString = gitHashString.toString('utf8').replace(/\s/g, '') + '-4'
-  } catch (e) {}
-
-  try {
-    gitHashString = readFileSync('../../../../VERSION')
-    gitHashString = gitHashString.toString('utf8').replace(/\s/g, '') + '-5'
-  } catch (e) {}
-
-  try {
-    gitHashString = readFileSync('../../../../../VERSION')
-    gitHashString = gitHashString.toString('utf8').replace(/\s/g, '') + '-6'
-  } catch (e) {}
-
   console.log('gitHashString:', gitHashString)
   return gitHashString
 }
