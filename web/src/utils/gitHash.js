2a366656994b94b4c77a5af3c23c43b4d80e1095
Paul Craig <paul_craig_16@hotmail.com>
2018-09-21 13:48:43 -0400

    fixup

--- web/src/utils/gitHash.js
+++ web/src/utils/gitHash.js
@@ -5,19 +5,24 @@ const { readFileSync } = require('fs')
 export default () => {
   let gitHashString = '0000000'
 
+  // try first to read from a version file
+  try {
+    gitHashString = readFileSync('./VERSION')
+    gitHashString = gitHashString.toString('utf8').replace(/\s/g, '')
+  } catch (e) {
+    // do nothing: no file was found
+  }
+
+  // if in a git repo, we can get the last commit hash directly (overrides VERSION file)
   try {
     gitHashString = execSync('git rev-parse --short HEAD', {
       stdio: ['pipe', 'pipe', null],
     })
       .toString()
       .trim()
-  } catch (e) {}
+  } catch (e) {
+    // do nothing: not in a git repo
+  }
 
-  try {
-    gitHashString = readFileSync('./VERSION')
-    gitHashString = gitHashString.toString('utf8').replace(/\s/g, '') + '-1'
-  } catch (e) {}
-
-  console.log('gitHashString:', gitHashString)
   return gitHashString
 }
