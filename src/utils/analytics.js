a3c7ca96fabfaec355453c541e64d579aff347e4
Tim Arney <timarney@users.noreply.github.com>
2019-01-15 17:20:46 -0500

    Disable GA (#527)

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,33 +1,21 @@
 import ReactGA from 'react-ga'
-import { windowExists } from '../utils/windowExists'
 
 export const initGA = ga_id => {
   ReactGA.initialize(ga_id)
 }
 
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname })
-  ReactGA.pageview(window.location.pathname)
+ return
 }
 
 export const logEvent = (category = '', action = '', label = '') => {
-  if (!windowExists() || !window.GA_INITIALIZED) return
-
-  if (category && action && label) {
-    ReactGA.event({ category, action, label })
-  }
+  return
 }
 
 export const logException = (description = '', fatal = false) => {
-  if (description) {
-    ReactGA.exception({ description, fatal })
-  }
+  return
 }
 
 export const trackRegistrationErrors = errObj => {
-  const errs = Object.keys(errObj).map(key => {
-    return key
-  })
-
-  logEvent('Registration', 'Submit', 'Error(s): ' + errs.join(', '))
+  return
 }
