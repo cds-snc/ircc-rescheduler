914cf02e0b1932e5eaf9a7ae9a988b1870442e3d
Paul Craig <paul_craig_16@hotmail.com>
2019-02-25 11:38:00 -0500

    Add test that confirms all months are included for a given location
    If you want to specifically ignore a month, then you can leave
    an empty array for that month.
    But we don't want people to forget about adding months.

--- test/has-properties.test.js
+++ test/has-properties.test.js
@@ -14,18 +14,45 @@ const checkLocation = function(obj) {
 }
 
 describe('Check location files for valid properties', () => {
-  it('it has email and receivingEmail properties', async () => {
-    const locations = require('require-all')({
-      dirname: path.resolve(__dirname, '../src/locations'),
-      filter: exclude,
-      excludeDirs: /^\.(__tests__)$/,
-      recursive: false,
-    })
+  const locations = require('require-all')({
+    dirname: path.resolve(__dirname, '../src/locations'),
+    filter: exclude,
+    excludeDirs: /^\.(__tests__)$/,
+    recursive: false,
+  })
 
+  it('it has email and receivingEmail properties', async () => {
     for (var key in locations) {
       // check properties on each location file
       /* eslint-disable security/detect-object-injection */
       checkLocation(locations[key])
+    }
+  })
+
+  it('it has all the months of the year in "recurring"', async () => {
+    const months = [
+      'jan',
+      'feb',
+      'mar',
+      'apr',
+      'may',
+      'jun',
+      'jul',
+      'aug',
+      'sep',
+      'oct',
+      'nov',
+      'dec',
+    ]
+
+    for (var key in locations) {
+      /* eslint-disable security/detect-object-injection */
+      let location = locations[key]
+
+      // check properties on each location file
+      expect(location).toHaveProperty('recurring')
+      let locationMonths = Object.keys(location.recurring)
+      expect(locationMonths.sort()).toEqual(months.sort())
     }
   })
 })
