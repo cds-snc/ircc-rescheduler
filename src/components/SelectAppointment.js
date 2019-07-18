cda1de98b9d292f9651ef9256d31e3ee40bf9abc
P0NDER0SA <michael.pond@hrsdc-rhdcc.gc.ca>
2019-07-18 08:47:44 -0400

    Trying to commit some sane code that passes eslint - this is our place holder page for appointments

--- src/components/SelectAppointment.js
+++ src/components/SelectAppointment.js
@@ -4,15 +4,15 @@ import rightArrow from '../assets/rightArrow.svg'
 import { arrow } from '../styles'
 import { Trans } from '@lingui/react'
 import FieldSet from './forms/FieldSet'
-import {  RadioAdapter} from '../components/forms/MultipleChoice'
+import { RadioAdapter } from '../components/forms/MultipleChoice'
 import { Form, Field } from 'react-final-form'
 import PropTypes from 'prop-types';
 
 
-const AppointmentForm = ({onSubmit}) => (
-    <Form onSubmit={onSubmit} render={({handleSubmit}) => (<form onSubmit={handleSubmit}>
+const AppointmentForm = ({ onSubmit }) => (
+    <Form onSubmit={onSubmit} render={({ handleSubmit }) => (<form onSubmit={ handleSubmit }>
         <div>
-            <FieldSet id="appointment" legendHidden={false} >
+            <FieldSet id="appointment" legendHidden={ false } >
                 <Field type="radio" component={RadioAdapter} label={<Trans>Dummy Appointment Time 1</Trans>} value="1" name="" id="app1" />
                 <Field type="radio" component={RadioAdapter} label={<Trans>Dummy Appointment Time 2</Trans>} value="2" name="" id="app2" />
                 <Field type="radio" component={RadioAdapter} label={<Trans>Dummy Appointment Time 3</Trans>} value="3" name="" id="app3" />
