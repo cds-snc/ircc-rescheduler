38618a8d37e9620c314d980a0a68d0561253508c
P0NDER0SA <michael.pond@hrsdc-rhdcc.gc.ca>
2019-07-30 15:27:04 -0400

    trying some fixes for kubernetes

---
+++ src/locations/sab-benrd.js
@@ -0,0 +1,21 @@
+module.exports = {
+  id: 'sab-benrd',
+  email: 'IRCC.DNCitMTLNotification-NotificationMTLCitRN.IRCC@cic.gc.ca',
+  phone: '1-888-111-1111',
+  receivingEmail:
+    'IRCC.DNCitMTLNotification-NotificationMTLCitRN.IRCC@cic.gc.ca',
+  recurring: {
+    jan: ['mon', 'tues'],
+    feb: ['mon', 'tues'],
+    mar: ['mon', 'tues'],
+    apr: ['mon', 'tues'],
+    may: ['mon', 'tues'],
+    jun: ['mon', 'tues'],
+    jul: ['mon', 'tues'],
+    aug: ['mon', 'tues'],
+    sep: ['mon', 'tues'],
+    oct: ['mon', 'tues'],
+    nov: ['mon', 'tues'],
+    dec: ['mon', 'tues'],
+  },
+}
