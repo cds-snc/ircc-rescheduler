c4568372e20a3ee2ccac6f9952b88fa811842197
kevinNghiem <kevin.nghiem@hrsdc-rhdcc.gc.ca>
2019-08-20 10:46:50 -0400

    removed highlighted weekdays on mon-tues

--- src/utils/calendarDates.js
+++ src/utils/calendarDates.js
@@ -16,7 +16,7 @@ import { getGlobalLocation } from '../locations/'
 import { Trans } from '@lingui/react'
 
 const offsetStartWeeks = 0
-const offsetEndWeeks = 4.5
+const offsetEndWeeks = 4.25
 
 export const toLocale = (date, options, locale) => {
   return makeGMTDate(format(date, 'YYYY-MM-DD')).toLocaleDateString(
