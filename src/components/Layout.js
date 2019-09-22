1ed483d19a1c5f4a09cf10f21ca141f22da2a516
kevinNghiem <kevin.nghiem@hrsdc-rhdcc.gc.ca>
2019-09-22 19:29:23 -0400

    eslint rm

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { css, injectGlobal } from 'emotion'
 import { theme, mediaQuery, content } from '../styles'
