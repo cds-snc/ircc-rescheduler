8bfaef2c1269d09e2c0038a1d655ef284b43d532
CalvinRodo <calvinv@gmail.com>
2019-09-26 10:32:07 -0400

    Adding checks for missing keys

--- src/email/sendmail.js
+++ src/email/sendmail.js
@@ -2,7 +2,13 @@ import { logError } from "../utils/logger" ;
 import { NotifyClient } from "notifications-node-client";
 
 const key = process.env.API_KEY;
+if (key === '' || typeof key === 'undefined') {
+  throw 'API_KEY environment variable not found'
+}
 const baseUrl = process.env.API_BASE_URL;
+if (baseUrl === '' || typeof baseUrl === 'undefined') {
+  throw 'API_BASE_URL environment variable not found'
+}
 const notifyClient =
   process.env.NODE_ENV != "test" ? new NotifyClient(baseUrl, key) : false;
 
