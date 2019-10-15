3a05d46763f9ecc8d9347555714ad541d028e4b8
will0684 <jordan.walcaraz@gmail.com>
2019-10-15 14:14:09 -0400

    Moved skip to describe

--- cypress/integration/calendar-page.spec.js
+++ cypress/integration/calendar-page.spec.js
@@ -27,7 +27,7 @@ function checkA11y(cy) {
   })
 }
 
-describe('Calendar page functions', () => {
+describe.skip('Calendar page functions', () => {
   beforeEach(() => {
     cy.visit('/calendar')
   })
@@ -90,7 +90,7 @@ describe('Calendar page functions', () => {
     // TODO: check for the rest of the text here
   })
 
-  it.skip('should find the first selectable day', () => {
+  it('should find the first selectable day', () => {
     // make sure we are on the right page
     cy.url().should('contains', '/calendar')
     // Compare today's date with the Day--today
